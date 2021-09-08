78e95f5a276f3680c4d5767a1f4fe156993f394b
ChristianAnagnostou <69920522+ChristianAnagnostou@users.noreply.github.com>
2021-09-08 14:14:07 -0700

    change duplicate class name

--- DesignPatterns/Creational/Prototype.js
+++ DesignPatterns/Creational/Prototype.js
@@ -2,7 +2,7 @@
  * Constructors with Prototypes
  */
 
-function Car(model, year, miles) {
+function Car2(model, year, miles) {
   this.model = model;
   this.year = year;
   this.miles = miles;
@@ -10,12 +10,12 @@ function Car(model, year, miles) {
 
 // Note here that we are using Object.prototype.newMethod rather than
 // Object.prototype so as to avoid redefining the prototype object
-Car.prototype.toString = function () {
+Car2.prototype.toString = function () {
   return this.model + " has done " + this.miles + " miles";
 };
 
-var civic = new Car("Honda Civic", 2009, 20000);
-var mondeo = new Car("Ford Mondeo", 2010, 5000);
+var civic = new Car2("Honda Civic", 2009, 20000);
+var mondeo = new Car2("Ford Mondeo", 2010, 5000);
 
 console.log(civic.toString()); // Honda Civic has done 20000 miles
 console.log(mondeo.toString()); // Ford Mondeo has done 5000 miles
